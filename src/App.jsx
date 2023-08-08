451171d3d89001f4e9d6908a3fe0358dcaaabe36
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-08-08 16:32:51 +0600

    add all daynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 import { useEffect, useState } from "react";
+import { FaPaperPlane } from "react-icons/fa";
+import { Grid } from "react-loader-spinner";
 import logo from "./assets/HODLINFO.8f78fc06.png";
 import BestPriceTrade from "./components/BestPriceTrade";
 import DataTable from "./components/DataTable";
 const App = () => {
   const [data, setData] = useState([]);
   const [cryptoName, setCryptoName] = useState("btc");
-
+  const [activeDark, setActiveDark] = useState(true);
+  const [countdown, setCountdown] = useState(60);
+  const yes = false;
   useEffect(() => {
     fetch(`http://localhost:5000/get-data/${cryptoName}`)
       .then((res) => res.json())
       .then((data) => setData(data[0]));
   }, [cryptoName]);
 
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      setCountdown((prevCountdown) =>
+        prevCountdown > 0 ? prevCountdown - 1 : 0
+      );
+    }, 1000);
+
+    return () => clearInterval(countdownInterval);
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      setCountdown(60); // Reset countdown to 60
+      // Refetch data after 1 minute
+      fetch(`http://localhost:5000/get-data/${cryptoName}`)
+        .then((res) => res.json())
+        .then((data) => setData(data[0]));
+    }
+  }, [countdown, cryptoName]);
   const { base_unit } = data;
   return (
     <div className="px-10 pt-5 dark:bg-[#191d28] min-h-screen">
@@ -37,22 +60,64 @@ const App = () => {
             <option value="btc" className="pt-3 w-11">
               BTC
             </option>
-            <option value="xrc">XRP</option>
+            <option value="xrp">XRP</option>
             <option value="eth">ETH</option>
+
+            <option value="trx">TRX</option>
+            <option value="eos">EOS</option>
+            <option value="zil">ZIL</option>
+            <option value="bat">BAT</option>
+            <option value="zrx">ZRX</option>
+            <option value="req">REQ</option>
+            <option value="nuls">NULS</option>
           </select>
 
           <button className="bg-black1 py-1 px-3 text-white rounded-md uppercase">
             BUY {base_unit}
           </button>
         </div>
-        <div>timer</div>
+        <div className="flex items-center gap-3">
+          <div className="rounded-full ring ring-p1 w-8 h-8 flex justify-center items-center text-p1">
+            {countdown}
+          </div>
+          <button className="bg-p1 rounded-lg text-white flex items-center px-4 py-1 gap-2 text-lg">
+            <FaPaperPlane /> Connect Telegram
+          </button>
+          <div
+            onClick={() => setActiveDark(!activeDark)}
+            className={`transition-all duration-300 w-14  h-8 rounded-2xl relative flex items-center px-1 cursor-pointer ${
+              activeDark ? "bg-black1" : "bg-white"
+            }`}
+          >
+            <div
+              className={`absolute w-6 h-6  bg-p1 rounded-full transition-all duration-300 ${
+                activeDark ? " translate-x-6" : " translate-x-0"
+              }`}
+            ></div>
+          </div>
+        </div>
       </header>
-      <main>
-        <BestPriceTrade />
-        <table className="w-full mt-5 ">
-          <DataTable data={data} />
-        </table>
-      </main>
+      {data ? (
+        <main>
+          <BestPriceTrade data={data} />
+          <table className="w-full mt-5 ">
+            <DataTable data={data} />
+          </table>
+        </main>
+      ) : (
+        <div className="flex justify-center items-center h-[calc(100vh-80px)]">
+          <Grid
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/BestPriceTrade.jsx
+++ src/components/BestPriceTrade.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 
-const BestPriceTrade = () => {
+const BestPriceTrade = ({ data }) => {
+  const { high, name, at, last, buy, sell } = data;
+  const difference = parseInt(last) - parseInt(buy);
+  const fiveMin = ((difference / parseInt(buy)) * 100).toFixed(2);
+  const oneHour = ((difference / parseInt(buy)) * 100).toFixed(2);
   return (
     <div className="flex justify-between items-center py-5 mt-5">
       <div className="text-center">
-        <h1 className="text-p1 text-[2.5rem]">0.74 %</h1>
+        <h1 className="text-p1 text-[2.5rem]"> {fiveMin}%</h1>
         <h4 className="dark:text-dark2 text-xl">5 Min</h4>
       </div>
       <div className="text-center">
-        <h1 className="text-p1 text-[2.5rem]">0.74 %</h1>
-        <h4 className="dark:text-dark2 text-xl">5 Min</h4>
+        <h1 className="text-p1 text-[2.5rem]">{fiveMin * 2} %</h1>
+        <h4 className="dark:text-dark2 text-xl">1 Hour</h4>
       </div>
       <div className="text-center">
         <h4 className="dark:text-dark2 text-3xl">Best Price to Trade</h4>
         <h1 className="dark:text-white text-[5rem]">
-          <span>₹ </span>26,24,110
+          <span>₹ </span>
+          {Math.ceil(parseInt(high)).toLocaleString()}
         </h1>
         <h6 className="dark:text-dark2 ">
-          Average BTC/INR net price including commission
+          Average {name} net price including commission
         </h6>
       </div>
       <div className="text-center">
-        <h1 className="text-p1 text-[2.5rem]">0.74 %</h1>
-        <h4 className="dark:text-dark2 text-xl">5 Min</h4>
+        <h1 className="text-p1 text-[2.5rem]">{(fiveMin * 12).toFixed(2)}%</h1>
+        <h4 className="dark:text-dark2 text-xl">1 Day</h4>
       </div>
       <div className="text-center">
-        <h1 className="text-p1 text-[2.5rem]">0.74 %</h1>
-        <h4 className="dark:text-dark2 text-xl">5 Min</h4>
+        <h1 className="text-p1 text-[2.5rem]">{(fiveMin * 24).toFixed(2)} %</h1>
+        <h4 className="dark:text-dark2 text-xl">7 Days</h4>
       </div>
     </div>
   );
